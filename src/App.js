25a47791708deff423cf39e947758244522fbdf6
Sandeeppk02 <sandy.pk0205@gmail.com>
2024-03-27 12:05:13 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculate-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+    <buttton className="span-two">AC</buttton>
+    <buttton>Del</buttton>
+    <buttton>÷</buttton>
+    <buttton>1</buttton>
+    <buttton>2</buttton>
+    <buttton>3</buttton>
+    <buttton>*</buttton>
+    <buttton>4</buttton>
+    <buttton>5</buttton>
+    <buttton>6</buttton>
+    <buttton>+</buttton>
+    <buttton>7</buttton>
+    <buttton>8</buttton>
+    <buttton>9</buttton>
+    <buttton>-</buttton>
+    <buttton>.</buttton>
+    <buttton>0</buttton>
+    <buttton className="span-two">=</buttton>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
